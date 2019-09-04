4e354a71595924a3c3c812d302e750c11157fc56
sebek9 <sebastian.kalinkowski@gmail.com>
2019-09-04 17:56:38 +0200

    Calculator

--- Calculator/src/main/java/com/kodilla/Calculator.java
+++ Calculator/src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,25 @@
 package com.kodilla;
 
-public class Calculator {
+public  class Calculator {
+
+    public int a=10;
+    public int b=5;
+    public double c;
+
+    public void addition(){
+        c=a+b;
+        System.out.println("Wynik dodawania: " + c);
+    }
+
+    public void substraction() {
+        c=a-b;
+        System.out.println("Wynik odejmowania: " + c);
+    }
+
+    public static void main(String []args){
+
+        Calculator calc = new Calculator();
+        calc.addition();
+        calc.substraction();
+    }
 }
