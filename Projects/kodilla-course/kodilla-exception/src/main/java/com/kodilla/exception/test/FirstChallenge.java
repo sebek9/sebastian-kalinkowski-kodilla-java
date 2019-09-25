e63ba1fdcd297a6b940ace05535e57bdbc7b3ed0
sebek9 <sebastian.kalinkowski@gmail.com>
2019-09-25 18:43:37 +0200

    Commit

---
+++ Projects/kodilla-course/kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,33 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if(b == 0){
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        try{ double result = firstChallenge.divide(3, 0);
+
+        }
+
+        catch (ArithmeticException e){
+
+            System.out.println("You cannot divide by zero.");
+            //System.out.println(result);
+        } finally{
+
+            System.out.println("Koniec programu");
+        }
+    }
+}
