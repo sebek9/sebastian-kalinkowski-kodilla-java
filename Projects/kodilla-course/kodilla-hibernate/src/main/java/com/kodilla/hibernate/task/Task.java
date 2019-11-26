876055e86515ded71ec222a21dc53b2005e33c3d
sebek9 <sebastian.kalinkowski@gmail.com>
2019-11-26 23:15:07 +0100

    17.1 17.2

---
+++ Projects/kodilla-course/kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -0,0 +1,63 @@
+package com.kodilla.hibernate.task;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+@Table(name = "TASKS")
+public final class Task {
+    private int id;
+    private String description;
+    private Date created;
+    private int duration;
+
+    public Task() {
+    }
+
+    public Task(String description, int duration) {
+        this.description = description;
+        this.created = new Date();
+        this.duration = duration;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    private void setDescription(String description) {
+        this.description = description;
+    }
+
+    @NotNull
+    @Column(name="CREATED")
+    public Date getCreated() {
+        return created;
+    }
+
+    private void setCreated(Date created) {
+        this.created = created;
+    }
+
+    @Column(name="DURATION")
+    public int getDuration() {
+        return duration;
+    }
+
+    private void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
