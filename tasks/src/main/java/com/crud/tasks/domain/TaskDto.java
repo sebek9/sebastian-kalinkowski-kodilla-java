9c2f1d39ded39c63e9afe06b7d2b0b9bc307504d
sebek9 <sebastian.kalinkowski@gmail.com>
2019-12-05 21:53:36 +0100

    18.3

--- tasks/src/main/java/com/crud/tasks/domain/TaskDto.java
+++ tasks/src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -4,10 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+
 public class TaskDto {
 
     private Long id;
     private String title;
     private String content;
+
+    public TaskDto(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
