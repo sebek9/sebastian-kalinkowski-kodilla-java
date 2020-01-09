fe8f2560f9e904f8623da536d4844d35e585738e
sebek9 <sebastian.kalinkowski@gmail.com>
2020-01-09 16:42:29 +0100

    #ticTacToe

--- tasks/src/main/java/com/crud/tasks/controller/TaskController.java
+++ tasks/src/main/java/com/crud/tasks/controller/TaskController.java
@@ -2,6 +2,9 @@ package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +16,12 @@ import java.util.List;
 @RequestMapping("/v1/task")
 
 public class TaskController {
+
+    @Autowired
+    private DbService service;
+    @Autowired
+    private TaskMapper taskMapper;
+
     @RequestMapping (method = RequestMethod.GET,value ="getTasks" )
     public List<TaskDto> getTasks(){
         return new ArrayList<>();
