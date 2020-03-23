2eceeaa4cf40d32b160f10eee72e30d740c6411e
sebastiankalinkowski <Polska500+>
2020-03-23 07:27:16 +0100

    24.3

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacadeWatcher.java
@@ -0,0 +1,33 @@
+package com.kodilla.patterns2.facade.api;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableAspectJAutoProxy
+public class OrderFacadeWatcher {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacadeWatcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public void logEventBefore() {
+        LOGGER.info("WATCHER: PROCESSING ORDER IN PROGRESS...");
+    }
+
+    @Around("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public Object measureTime(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object result;
+        try {
+            result = proceedingJoinPoint.proceed();
+            LOGGER.info("WATCHER: ORDER SUCCEEDED!");
+        } catch (Throwable throwable) {
+            LOGGER.error("WATCHER: ORDER FAILED: " + throwable.getMessage());
+            throw throwable;
+        }
+        return result;
+    }
+}
